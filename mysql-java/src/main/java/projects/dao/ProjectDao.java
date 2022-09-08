5771dbc98f39e678002df8ab0db6178b6e8a3532
Ranon Pador <108454239+RPador@users.noreply.github.com>
2022-09-08 15:47:13 -0700

    Add files via upload

--- mysql-java/src/main/java/projects/dao/ProjectDao.java
+++ mysql-java/src/main/java/projects/dao/ProjectDao.java
@@ -177,7 +177,6 @@ private List<Category> fetchCategoriesForProject(Connection conn, Integer projec
 		
 		try(ResultSet rs = stmt.executeQuery()) {
 			List<Category> categories = new LinkedList<Category>();
-			
 			while(rs.next()) {
 				categories.add(extract(rs, Category.class));
 			} return categories;
@@ -185,5 +184,66 @@ private List<Category> fetchCategoriesForProject(Connection conn, Integer projec
 	}
 }
 
+
+public boolean modifyProjectDetails(Project project) {
+	String sql = ""
+		+"UPDATE " + PROJECT_TABLE + " SET "
+		+"project_name = ?, "
+		+"estimated_hours = ?, "
+		+"actual_hours =?, "
+		+"difficulty = ?, "
+		+"notes = ? "
+		+"WHERE project_id = ? ";
+	
+	try(Connection conn = DbConnection.getConnection()) {
+		startTransaction(conn);
+		
+		try(PreparedStatement stmt = conn.prepareStatement(sql)) {
+			setParameter(stmt, 1, project.getProjectName(), String.class);
+			setParameter(stmt, 2, project.getEstimatedHours(), BigDecimal.class);
+			setParameter(stmt, 3, project.getActualHours(), BigDecimal.class);
+			setParameter(stmt, 4, project.getDifficulty(), Integer.class);
+			setParameter(stmt, 5, project.getNotes(), String.class);
+			setParameter(stmt, 6, project.getProjectId(), Integer.class);
+			
+			boolean modified = stmt.executeUpdate() == 1;
+			commitTransaction(conn);
+			
+			return modified;
+		}
+		catch(Exception e) {
+			rollbackTransaction(conn);
+			throw new DbException(e);
+		}
+	}
+	catch(SQLException e) {
+		throw new DbException(e);
+	}
+}
+
+
+public boolean deleteProject(Integer projectId) {
+	String sql = "DELETE FROM " + PROJECT_TABLE + " WHERE project_id = ?";
+			
+			try(Connection conn = DbConnection.getConnection()) {
+				startTransaction(conn);
+				
+				try(PreparedStatement stmt = conn.prepareStatement(sql)) {
+					setParameter(stmt, 1, projectId, Integer.class);
+					
+					boolean deleted = stmt.executeUpdate() == 1;
+					
+					commitTransaction(conn);
+					return deleted;
+				}
+				catch(Exception e) {
+					rollbackTransaction(conn);
+					throw new DbException(e);
+				}
+}
+			catch(SQLException e) {
+				throw new DbException(e);
+			}
+}
 }
 
